d338594af6e9fea489efbce470054439ba742e76
PIYUSH TIWARI <piyushayush822114@gmail.com>
2026-07-08 19:41:24 +0530

    Add sample repository data for README tests
    Added sample repository data for testing README generation.

--- test_readme_generator.py
+++ test_readme_generator.py
@@ -5,3 +5,17 @@ sys.path.insert(0, 'e:\\RepoPilotAI')
 from services.readme_generator import generate_readme
 
 # Sample repository data
+sample_repo_data = {
+    'name': 'awesome-project',
+    'owner': 'john-doe',
+    'description': 'An awesome open-source project for developers',
+    'url': 'https://github.com/john-doe/awesome-project',
+    'language': 'Python',
+    'topics': ['python', 'api', 'web', 'rest'],
+    'stars': 1250,
+    'forks': 89,
+    'license': 'MIT',
+    'homepage': 'https://awesome-project.com',
+    'visibility': 'Public'
+}
+
